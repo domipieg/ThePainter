3728b1b85f77a0e0270cd672f8a5ec2c913ecc20
Dominik <domi.pieg@gmail.com>
2016-03-17 19:21:19 +0100

    modyfied

--- Paint/App.xaml.cs
+++ Paint/App.xaml.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Windows;
-
-namespace Paint2
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
+using System.Windsad
     }
 }
